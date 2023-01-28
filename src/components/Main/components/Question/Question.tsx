250b22364342251504c64071f805c75e66b007a0
stachkrol <st.krolikiewicz@gmail.com>
2023-01-28 19:31:54 +0100

    delete OpenTDb class, add new functions & button

--- src/components/Main/components/Question/Question.tsx
+++ src/components/Main/components/Question/Question.tsx
@@ -1,21 +1,42 @@
 import React, { useEffect, useState } from "react";
-import OpentdbService from "../../../../services/opentdb.service";
+import { getQuestion } from "../../../../services/opentdb.service";
 
 const Question = () => {
-    const opentdb = new OpentdbService();
     const [question, setQuestion] = useState("");
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
-        opentdb.getQuestion().then((data) => {
-            setQuestion(data);
-        });
+        getQuestion()
+            .then((res) => {
+                setQuestion(res);
+            })
+            .then(() => {
+                setLoading(false);
+            });
     }, []);
 
+    const handleClick = () => {
+        setLoading(true);
+        getQuestion()
+            .then((res) => {
+                setQuestion(res);
+            })
+            .then(() => {
+                setLoading(false);
+            });
+    };
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-12">
-                    <h1>Question</h1>
-                    {question === "" ? <p>Loading...</p> : <p>{question}</p>}
+                    <h2>Question:</h2>
+                    {loading ? <p>Loading...</p> : <p>{question}</p>}
+                    <button
+                        className="btn btn-primary"
+                        onClick={handleClick}
+                        disabled={loading}
+                    >
+                        {loading ? "Loading..." : "Get new question"}
+                    </button>
                 </div>
             </div>
         </div>
--- src/services/opentdb.service.ts
+++ src/services/opentdb.service.ts
@@ -1,14 +1,23 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 
-export default class OpentdbService {
-    async getQuestion(): Promise<string> {
-        const rawResponse = await axios.get(
-            `https://opentdb.com/api.php?amount=1`
-        );
-        const response = rawResponse.data.results[0].question
+export async function getQuestion(): Promise<string> {
+    const response = await axios.get(`https://opentdb.com/api.php?amount=1`);
+    const question = response.data.results[0].question
+        .replace(/&quot;/g, '"')
+        .replace(/&#039;/g, "'")
+        .replace(/&eacute;/g, "é");
+    console.log(question);
+    return question;
+}
+
+export async function getQuiz(): Promise<string[]> {
+    const response = await axios.get(`https://opentdb.com/api.php?amount=2`);
+    const quiz = response.data.results.map((question: any) => {
+        return question.question
             .replace(/&quot;/g, '"')
-            .replace(/&#039;/g, "'");
-        console.log(response);
-        return response;
-    }
+            .replace(/&#039;/g, "'")
+            .replace(/&eacute;/g, "é");
+    });
+    console.log(quiz);
+    return quiz;
 }
