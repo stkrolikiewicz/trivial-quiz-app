91d4b0f4db4ffe13d04b220787ca0657aa32b3f7
stachkrol <st.krolikiewicz@gmail.com>
2023-01-29 14:25:49 +0100

    implementing Category and Question interfaces

--- src/services/opentdb.service.ts
+++ src/services/opentdb.service.ts
@@ -1,17 +1,21 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
+import Category from "../types/category.type";
+import Question from "../types/question.type";
 
-export async function getQuestion(): Promise<string> {
+export async function getQuestion(): Promise<Question> {
     const response = await axios.get(`https://opentdb.com/api.php?amount=1`);
-    const question = response.data.results[0].question
+    response.data.results[0].question = response.data.results[0].question
         .replace(/&quot;/g, '"')
         .replace(/&#039;/g, "'")
         .replace(/&eacute;/g, "é");
+
+    const question = response.data.results[0];
     console.log(question);
     return question;
 }
 
-export async function getQuiz(): Promise<string[]> {
-    const response = await axios.get(`https://opentdb.com/api.php?amount=2`);
+export async function getQuiz(): Promise<Question[]> {
+    const response = await axios.get(`https://opentdb.com/api.php?amount=10`);
     const quiz = response.data.results.map((question: any) => {
         return question.question
             .replace(/&quot;/g, '"')
@@ -20,4 +24,11 @@ export async function getQuiz(): Promise<string[]> {
     });
     console.log(quiz);
     return quiz;
+}
+
+export async function getCategories(): Promise<Category[]> {
+    const response = await axios.get(`https://opentdb.com/api_category.php`);
+    const categories = response.data.trivia_categories;
+    console.log(categories);
+    return categories;
 }
