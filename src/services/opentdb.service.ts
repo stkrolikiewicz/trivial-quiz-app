ac9ffd72f95ab664236e02de83dff05a20acdc6b
stachkrol <st.krolikiewicz@gmail.com>
2023-01-29 20:43:30 +0100

    updating Qeustion interface & getQuiz function

--- src/services/opentdb.service.ts
+++ src/services/opentdb.service.ts
@@ -14,13 +14,30 @@ export async function getQuestion(): Promise<Question> {
     return question;
 }
 
-export async function getQuiz(): Promise<Question[]> {
-    const response = await axios.get(`https://opentdb.com/api.php?amount=10`);
-    const quiz = response.data.results.map((question: any) => {
-        return question.question
-            .replace(/&quot;/g, '"')
-            .replace(/&#039;/g, "'")
-            .replace(/&eacute;/g, "é");
+export async function getQuiz(category: string): Promise<Question[]> {
+    const response = await axios.get(
+        `https://opentdb.com/api.php?amount=10&category=${category}`
+    );
+    const quiz = response.data.results.map((element: any) => {
+        return {
+            ...element,
+            question: element.question
+                .replace(/&quot;/g, '"')
+                .replace(/&#039;/g, "'")
+                .replace(/&eacute;/g, "é"),
+            answers: [
+                ...element.incorrect_answers.map((answer: string) =>
+                    answer
+                        .replace(/&quot;/g, '"')
+                        .replace(/&#039;/g, "'")
+                        .replace(/&eacute;/g, "é")
+                ),
+                element.correct_answer
+                    .replace(/&quot;/g, '"')
+                    .replace(/&#039;/g, "'")
+                    .replace(/&eacute;/g, "é"),
+            ].sort((a, b) => 0.5 - Math.random()),
+        };
     });
     console.log(quiz);
     return quiz;
